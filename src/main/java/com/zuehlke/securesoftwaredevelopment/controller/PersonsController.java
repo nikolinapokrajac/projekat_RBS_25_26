88eaee69934c2e0fc43d7ec7c426cf27af99da77
nikolinapokrajac <pokrajacnikolina3@gmail.com>
2026-03-14 02:03:03 +0100

    to app_sa_svim_odbranama

--- src/main/java/com/zuehlke/securesoftwaredevelopment/controller/PersonsController.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/controller/PersonsController.java
@@ -10,10 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-<<<<<<< HEAD
-=======
 import org.springframework.security.access.prepost.PreAuthorize;
->>>>>>> implementacija_autorizacije
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
